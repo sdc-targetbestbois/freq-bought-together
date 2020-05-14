00b1f5ae5593bc7910a578d333e3f12956a7c9c1
George Anthony Pazdral II <jorge.antonio.de.segundo@gmail.com>
2020-05-14 15:47:29 -0500

    Delete config.js from .gitignore

---
+++ database/config.js
@@ -0,0 +1,7 @@
+const mysqlConfig = {
+    host: 'databasegeorgefec.cqibdsqfin7v.us-east-2.rds.amazonaws.com',
+    user: 'root',
+    password: 'password',
+    database: 'products'
+  };
+  module.exports = { mysqlConfig };
